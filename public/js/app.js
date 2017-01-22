e5dc928dd3097801fd807776fd7ceb62850ded6f
RobertAKARobin <robertgfthomas@gmail.com>
2017-01-22 02:47:02 -0600

    Can add and delete items

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,41 @@
 'use strict';
 
-firebase.initializeApp({
-	databaseURL: 'ws://127.0.1:5000'
-});
+var fb = (function(){
+	firebase.initializeApp({
+		databaseURL: 'ws://127.0.1:5000'
+	});
+	return firebase.database();
+})();
 
-document.addEventListener('DOMContentLoaded', function(){
-	new Vue({
-		el: '#app',
+var Item = (function(){
+	var pub = {};
+	var ref = {};
+	var vue = new Vue({
 		data: {
-			message: 'Vue is alive!'
+			all: [],
+			blank: {}
+		},
+		methods: {
+			create: function(){
+				var vue = this;
+				ref.push(vue.blank);
+				vue.blank = {};
+			},
+			destroy: function(item){
+				var vue = this;
+				ref.child(item['.key']).remove();
+			}
 		}
 	});
+
+	pub.loadAndMountTo = function(selector){
+		ref = fb.ref('/items');
+		vue.$bindAsArray('all', ref);
+		vue.$mount(selector);
+	}
+	return pub;
+})();
+
+document.addEventListener('DOMContentLoaded', function(){
+	Item.loadAndMountTo('#items');
 });
