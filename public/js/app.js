e1d4a1ca5c57bcb0dd3dd61adba3f24940f4d924
RobertAKARobin <robertgfthomas@gmail.com>
2017-02-09 20:17:20 -0600

    Vuefire and Router skeleton

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,36 @@
 'use strict';
 
-firebase.initializeApp({
+var FB,
+	ConvoList,
+	Router;
+
+FB = firebase.initializeApp({
 	databaseURL: 'ws://127.0.1:5000'
+}).database();
+
+ConvoList = Vue.component('convoList', {
+	template: '#convoList'
+});
+
+Router = new VueRouter({
+	routes: [
+		{
+			path: '/',
+			name: 'convoList',
+			component: ConvoList
+		}
+	]
 });
 
 document.addEventListener('DOMContentLoaded', function(){
 	new Vue({
 		el: '#app',
-		data: {
-			message: 'Vue is alive!'
+		router: Router
+	});
+	new Vue({
+		el: '#console',
+		firebase: {
+			all: FB.ref('/')
 		}
 	});
 });
