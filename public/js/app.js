bf5080ce9ec2567c1d9567c97ecb74a70f3da8df
RobertAKARobin <robertgfthomas@gmail.com>
2017-02-09 20:38:56 -0600

    Can add new Convos

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,8 @@
 
 var FB,
 	ConvoList,
+	ConvoFormNew,
+	Convo,
 	Router;
 
 FB = firebase.initializeApp({
@@ -9,7 +11,42 @@ FB = firebase.initializeApp({
 }).database();
 
 ConvoList = Vue.component('convoList', {
-	template: '#convoList'
+	template: '#convoList',
+	firebase: {
+		convos: FB.ref('/convos')
+	}
+});
+
+ConvoFormNew = Vue.component('convoFormNew', {
+	template: '#convoFormNew',
+	data: function(){
+		return {
+			db: {
+				title: Math.round(Date.now() / 1000)
+			}
+		}
+	},
+	methods: {
+		create: function(){
+			var input = this;
+			FB.ref('/convos').push(input.db);
+			Object.assign(input.$data, ConvoFormNew.options.data.call(input));
+		}
+	}
+});
+
+Convo = Vue.component('convo', {
+	template: '#convo',
+	props: ['fbid'],
+	firebase: function(){
+		var convo = this;
+		return {
+			db: {
+				source: FB.ref('/convos').child(convo.fbid),
+				asObject: true
+			}
+		}
+	}
 });
 
 Router = new VueRouter({
